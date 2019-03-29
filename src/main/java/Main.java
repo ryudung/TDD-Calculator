d5794ffeea9121ae33a4b450238e56b5e2edac4c
ryudung <rhkdgus221@naver.com>
2019-03-29 11:29:55 +0900

    todo 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,29 @@ import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * TODO:
+     * 요구사항 1단계
+     *
+     * - input 을 입력 받는다.
+     *
+     * 1. input 값을 파싱한다.
+     *  - "1+2-3"
+     *      -> {"1", "2", "3"}
+     *      -> {"+", "-"}
+     *
+     * 2. calc()
+     * - 연산자로 계산한다.
+     *  - 더하기
+     *  - 빼기
+     *  - 곱하기
+     *  - 나누기
+     *
+     * 3.
+     * - 결과값을 가지고 출력한다.
+     *
+     * */
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
