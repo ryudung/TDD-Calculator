b06e20ce481d7018e4837d0e4abc51132ebb53db
ryudung <rhkdgus221@naver.com>
2019-03-29 18:25:19 +0900

    프린터 추가, READMD.md 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import calc.Calculator;
+import parser.Parser;
+import parser.data.ParsedData;
+import printer.Printer;
+
 import java.util.Scanner;
 
 public class Main {
@@ -5,25 +10,24 @@ public class Main {
     /**
      * TODO:
      * 요구사항 1단계
-     *
+     * <p>
      * - input 을 입력 받는다.
-     *
+     * <p>
      * 1. input 값을 파싱한다.
-     *  - "1+2-3"
-     *      -> {"1", "2", "3"}
-     *      -> {"+", "-"}
-     *
+     * - "1+2-3"
+     * -> {"1", "2", "3"}
+     * -> {"+", "-"}
+     * <p>
      * 2. calc()
      * - 연산자로 계산한다.
-     *  - 더하기
-     *  - 빼기
-     *  - 곱하기
-     *  - 나누기
-     *
+     * - 더하기
+     * - 빼기
+     * - 곱하기
+     * - 나누기
+     * <p>
      * 3.
      * - 결과값을 가지고 출력한다.
-     *
-     * */
+     */
 
     public static void main(String[] args) {
 
@@ -36,6 +40,18 @@ public class Main {
         //======작성 시작=======//
 
 
+        //파싱
+        Parser parser = new Parser();
+        ParsedData parserData = parser.parsing(input);
+
+        //계산
+        Calculator calc = new Calculator();
+        int result = calc.calc(parserData);
+
+
+        //프린터
+        Printer printer = new Printer();
+        printer.print(result);
 
     }
 }
---
+++ src/main/java/printer/Printer.java
@@ -0,0 +1,8 @@
+package printer;
+
+public class Printer {
+
+    public void print(int result) {
+        System.out.println("결과값:" + result);
+    }
+}
