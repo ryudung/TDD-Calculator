15e99b54dbbdda3acf0288fec9c0dbaf89ace18f
ryudung <rhkdgus221@naver.com>
2019-03-29 22:06:57 +0900

    numbers 주석 추가

--- src/main/java/parser/data/Numbers.java
+++ src/main/java/parser/data/Numbers.java
@@ -13,11 +13,30 @@ public class Numbers {
         return new Numbers(numbers);
     }
 
+    /**
+     * 첫번째 값을 리턴하는 메서드.
+     *
+     * @return 첫번째 숫자
+     */
     public int findFirst() {
         return numbers.get(0);
     }
 
+    /**
+     * 해당위치 값을 리턴하는 메서드.
+     *
+     * @return 해당위치 숫자
+     */
     public int get(int i) {
         return numbers.get(i);
+    }
+
+    /**
+     * 다음 값을 리턴하는 메서드.
+     *
+     * @return 다음 숫자
+     */
+    public int next(int i) {
+        return numbers.get(i + 1);
     }
 }
